27e88e3de28079bcb9ad1859b9034a68ecbc52f6
Joel Bohorquez <19965325+joelbqz@users.noreply.github.com>
2023-10-21 02:44:30 -0500

    Add files via upload

---
+++ _next/static/sB5pOjDzqNIERljw3bb-n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/cac9b645e8d7e8c8.css","static/chunks/pages/index-e9d7347007ed229c.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
